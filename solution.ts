ab8ccf3df93f796fc35d6f3b062a7382f2cbe13a
Alif-Sarker <sarker202454@gmil.com>
2025-11-17 11:54:44 +0600

    problem 4 solved

--- solution.ts
+++ solution.ts
@@ -47,3 +47,25 @@ class Person {
         return `Name: ${this.name}, Age: ${this.age}`;
     }
 }
+
+
+
+
+
+type Book = {
+    title : string;
+    rating : number;
+}
+
+function filterByRating(items : Book[]):Book[] {
+
+    return (items.filter( item => item.rating >= 4 ));
+}
+
+
+const books = [
+  { title: 'Book A', rating: 4.5 },
+  { title: 'Book B', rating: 3.2 },
+  { title: 'Book C', rating: 5.0 },
+];
+
