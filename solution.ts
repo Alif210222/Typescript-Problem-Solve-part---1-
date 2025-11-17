431230d63fdf361cafe478620353878479f9ec9b
Alif-Sarker <sarker202454@gmil.com>
2025-11-17 12:37:52 +0600

    problem 5 solved

--- solution.ts
+++ solution.ts
@@ -62,10 +62,31 @@ function filterByRating(items : Book[]):Book[] {
     return (items.filter( item => item.rating >= 4 ));
 }
 
-
 const books = [
   { title: 'Book A', rating: 4.5 },
   { title: 'Book B', rating: 3.2 },
   { title: 'Book C', rating: 5.0 },
+];
+
+
+
+
+
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    isActive:boolean;
+}
+
+function filterActiveUsers (array : User[]): User[] {
+         return array.filter(user => user.isActive === true )
+}
+
+const users = [
+  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
 ];
 
